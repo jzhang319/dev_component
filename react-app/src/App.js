2fe8eaa5776c0c7c0c42f9d77bc8962e80d21d0a
hensell de la rosa <hensell1234@gmail.com>
2024-04-27 23:39:42 -0400

    created export file

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -4,6 +4,7 @@ import { Route, Switch } from "react-router-dom";
 import SignupFormPage from "./components/SignupFormPage";
 import LoginFormPage from "./components/LoginFormPage";
 import { authenticate } from "./store/session";
+import { Home } from "./exports";
 // import Navigation from "./components/Navigation";
 
 function App() {
@@ -23,6 +24,10 @@ function App() {
           </Route>
           <Route path="/signup">
             <SignupFormPage />
+          </Route>
+
+          <Route path="/">
+            <Home/>
           </Route>
         </Switch>
       )}
---
+++ react-app/src/exports.js
@@ -0,0 +1,7 @@
+// pages export
+export {default as Login} from './pages/login/Login'
+export {default as Home} from './pages/home/Home'
+
+
+// component export
+
