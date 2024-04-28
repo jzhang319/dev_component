dcf4f8580767ac8428548d545db1cbe261fa4c13
hensell de la rosa <hensell1234@gmail.com>
2024-04-27 22:40:42 -0400

    set up global properties

--- react-app/src/index.js
+++ react-app/src/index.js
@@ -8,7 +8,7 @@ import configureStore from "./store";
 import * as sessionActions from "./store/session";
 import App from "./App";
 
-import "./index.css";
+import "./index.scss";
 
 const store = configureStore();
 
