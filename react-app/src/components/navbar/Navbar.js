8c3f639c4d853fe963a552436cbe302ff55fae2c
jzhang319 <jzhang319@gmail.com>
2024-04-28 13:41:37 -0400

    reinstrall tailwind

--- react-app/src/components/navbar/Navbar.js
+++ react-app/src/components/navbar/Navbar.js
@@ -1,15 +1,14 @@
-import React from 'react'
-import './Navbar.scss'
-import Avatar from '@mui/material/Avatar'
-import {SearchIcon} from '../../exports'
+import React from "react";
+import "./Navbar.scss";
+import Avatar from "@mui/material/Avatar";
+import { SearchIcon } from "../../exports";
 
-const Navbar = ({sessionUser}) => {
-
+const Navbar = ({ sessionUser }) => {
   let content;
 
   return (
-    <div className='navbar'>
-      <div className="navbar__wrapper">
+    <div className="navbar">
+      <div className="navbar__wrapper sm:flex-col">
         <div className="navbar__left">
           <h1 className="navbar__left--logo">Dev</h1>
           <h1 className="navbar__left--logo2">Component</h1>
@@ -17,25 +16,28 @@ const Navbar = ({sessionUser}) => {
 
         <div className="navbar__center">
           <input
-          type="text"
-          className="navbar__center--input"
-          placeholder='Search Components Here ...'
+            type="text"
+            className="navbar__center--input"
+            placeholder="Search Components Here ..."
           />
 
-          <SearchIcon className='navbar__center--search'
-          style={{fontSize: '2.5rem', fontWeight:'700'}}/>
+          <SearchIcon
+            className="navbar__center--search"
+            style={{ fontSize: "2.5rem", fontWeight: "700" }}
+          />
         </div>
 
         <div className="navbar__right">
           {/* <span>hello</span>
           <span>hello</span> */}
-          <span className='navbar__right--username'>{sessionUser?.username} Demo</span>
-          <Avatar/>
+          <span className="navbar__right--username">
+            {sessionUser?.username} Demo
+          </span>
+          <Avatar />
         </div>
       </div>
-
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
