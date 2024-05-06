fc77a7f2f31a013b73d6b0dbecf120fc6168e3ca
jzhang319 <jzhang319@gmail.com>
2024-05-06 18:17:50 -0400

    fixing files in seeds, and models

--- migrations/versions/20201120_150602_create_users_table.py
+++ migrations/versions/20201120_150602_create_users_table.py
@@ -30,6 +30,7 @@ def upgrade():
     sa.UniqueConstraint('email'),
     sa.UniqueConstraint('username')
     )
+    
     if environment == "production":
         op.execute(f"ALTER TABLE users SET SCHEMA {SCHEMA};")
     # ### end Alembic commands ###qqqqqqqqq
