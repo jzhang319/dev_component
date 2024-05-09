fad6a35219bdd1c0ebf96f0c9b483824b77a6815
hensell de la rosa <hensell1234@gmail.com>
2024-05-08 22:33:21 -0400

    avatar added

--- react-app/src/components/createComponent/CreateComponent.js
+++ react-app/src/components/createComponent/CreateComponent.js
@@ -1,24 +1,30 @@
 import React, {useState} from 'react'
 import {addComponentThunk} from '../../store/component'
-import './_CreateComponent.scss'
 import { useSelector } from 'react-redux'
+import { Avatar } from '@mui/material'
+
 const CreateComponent = () => {
   const sessionUser = useSelector(state => state.session.user)
 
    const [type, setType] = useState('')
-   const [c, setC] = useState('')
-   const [i, setI] = useState('')
-   const [first, setfirst] = useState('')
+   const [code, setCode] = useState('')
+   const [image, setImage] = useState('')
+  //  const [first, setfirst] = useState('')
   return (
     <div className='createComponent'>
       <div className="createComponent__wrapper">
 
         <div className="createComponent__title">
-          <h1 className="createComponent__logo1">Dev</h1>
-          <h1 className="createComponent__logo2">Component</h1>
+          <h1 className="logo1">Dev</h1>
+          <h1 className="logo2">Component</h1>
         </div>
 
-        <div className="createComponrnt__formField">
+        <div className="createComponent__userInfo">
+          <Avatar className='createComponent__avatar' src='images/me.jpg'/>
+          <h3 className="createComponent__username">{sessionUser?.username}</h3>
+        </div>
+
+        <div className="createComponent__formField">
           <form action="" className="createComponent__form">
             <input
             type="text"
